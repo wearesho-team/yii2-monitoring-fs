cdf5cabcaee409c6d23be9c6d16ce7165ffc8fb4
Kartvaik <roman.varkuta@gmail.com>
2019-02-19 12:16:18 +0200

    Implementation

---
+++ tests/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+
+\Yii::setAlias(
+    '@Wearesho\Yii\Monitoring',
+    \dirname(__DIR__) . DIRECTORY_SEPARATOR . 'src'
+);
+\Yii::setAlias('@configFile', __DIR__ . DIRECTORY_SEPARATOR . 'config.php');
